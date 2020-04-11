5ddac304d7fa52fa8ff65e2d434f9d5c54394ed7
Danny Shen <shenfive@gmail.com>
2020-04-11 16:26:59 +0800

    name

--- HelloAPI0411/ViewController.swift
+++ HelloAPI0411/ViewController.swift
@@ -7,24 +7,37 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var can: UIView!
+    @IBOutlet weak var header: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var email: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+    }
+
+    @IBAction func update(_ sender: Any) {
         let model = APIModel.share
 
         model.queryRandomUserAlamofire { (res, error) in
             if error != nil{
-                print(error?.localizedDescription)
+                print(error!.localizedDescription)
                 return
             }
-            print(res as? Data)
+            if let data = res as? Data{
+                let json = JSON(data)
+                print(json)
+                self.email.text = json["results"][0]["email"].stringValue
+            }
             
             
         }
     }
-
-
+    
 }
 
